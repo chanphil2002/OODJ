5d8d255ec0a97eda9a2f7c77c404deccd3a20bc4
hang5 <hang5@LAPTOP-DQPEUCQE>
2023-09-08 22:43:28 +0800

    First Commit!

--- OODJ/src/view/PurchaseManagerMenu.java
+++ OODJ/src/view/PurchaseManagerMenu.java
@@ -4,10 +4,71 @@
  */
 package view;
 
-/**
- *
- * @author pc
- */
+import controller.CreateEntry;
+import controller.DisplayEntry;
+import controller.EditEntry;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class PurchaseManagerMenu {
-    
-}
+    public void menu(){
+        System.out.println("Puchase Manager Menu");
+        List<String> options  = new ArrayList<>();
+        options.add("List of items");
+        options.add("List of suppliers");
+        options.add("Display requisition");
+        options.add("Generate purchase order");
+        options.add ("List of purchaser order");
+        options.add("Log out");
+        int option;
+        option = OptionPicker.optionPicker(options);
+        switch (option){
+            case 1:
+                DisplayEntry.displayItemsEntry();
+                menu();
+                break;
+                
+            case 2:
+                DisplayEntry.displaySupplierEntry();
+                menu();
+                break;
+           
+            case 3:
+                DisplayEntry.displayPREntry();
+                menu();
+                break;
+            case 4:
+                managePOMenu();
+                break;
+            case 5:
+                break;
+        }             
+                
+        }
+
+    private void managePOMenu() {
+        DisplayEntry.displayPREntry();
+        List<String> options = new ArrayList<String>();
+        options.add("Create Purchase Requisition");
+        options.add("Edit/Delete Purchase Requisition");
+        options.add("Return to Purchase Manager Menu");
+        int option = OptionPicker.optionPicker(options);
+        switch(option){
+            case 1:
+                CreateEntry.createPREntry();
+                managePOMenu();
+                break;
+            case 2:
+                EditEntry.editPREntry();
+                managePOMenu();
+                break;
+            case 3 :
+                menu();
+                break;
+        }
+        
+    }
+
+    }
+ 
